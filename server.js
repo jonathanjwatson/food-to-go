59222c69e76aea2f9cb6c8ea7b306eedbd30c160
Jonathan Watson <jonathanwatson@Jonathans-MacBook-Pro.local>
2020-07-10 09:28:09 -0400

    initial boilerplate setup

--- server.js
+++ server.js
@@ -1,13 +1,3 @@
-# food-to-go
-
-## Setup
-
-1. `touch server.js`
-2. `npm init -y`
-3. `npm install express express-handlebars@3.0.0`
-4. Copy boilerplate express/handlebars code into `express.js`
-
-```javascript
 const express = require("express");
 const exphbs = require("express-handlebars");
 
@@ -26,8 +16,3 @@ app.use(express.json());
 app.listen(PORT, function () {
   console.log(`Server listening on: http://localhost:${PORT}`);
 });
-```
-
-## Deploy to Heroku
-1. Ensure you are on the master branch
-2. 
